4a618d376001015f63dcea03230cfc8a57ad0358
huanglong <huanglong@bytedance.com>
2015-11-30 19:44:42 +0800

    spooler: remove setpriority
    avoid starvation under high load
    
    Change-Id: Ibf87b5a3e633b079fdce2583f6f1e65a969322b1

--- core/spooler.c
+++ core/spooler.c
@@ -396,17 +396,17 @@ void spooler(struct uwsgi_spooler *uspool) {
 	int nullfd;
 
 	// asked by Marco Beri
-#ifdef __HAIKU__
-#ifdef UWSGI_DEBUG
-	uwsgi_log("lowering spooler priority to %d\n", B_LOW_PRIORITY);
-#endif
-	set_thread_priority(find_thread(NULL), B_LOW_PRIORITY);
-#else
-#ifdef UWSGI_DEBUG
-	uwsgi_log("lowering spooler priority to %d\n", PRIO_MAX);
-#endif
-	setpriority(PRIO_PROCESS, getpid(), PRIO_MAX);
-#endif
+//#ifdef __HAIKU__
+//#ifdef UWSGI_DEBUG
+//	uwsgi_log("lowering spooler priority to %d\n", B_LOW_PRIORITY);
+//#endif
+//	set_thread_priority(find_thread(NULL), B_LOW_PRIORITY);
+//#else
+//#ifdef UWSGI_DEBUG
+//	uwsgi_log("lowering spooler priority to %d\n", PRIO_MAX);
+//#endif
+//	setpriority(PRIO_PROCESS, getpid(), PRIO_MAX);
+//#endif
 
 	nullfd = open("/dev/null", O_RDONLY);
 	if (nullfd < 0) {
